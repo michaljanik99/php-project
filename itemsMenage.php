f3f7ed0eff2a71481c16d5bebd25b62d496a6123
mjanik <mjanik@gravityglobal.com>
2022-01-29 21:34:33 +0100

    add edit part 6

--- itemsMenage.php
+++ itemsMenage.php
@@ -29,7 +29,13 @@ if(isset($_POST['passEdit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $url = $_POST['url'];
-    mysqli_query($serwer, "update passtable set Name='$name', `e-mail`='$email', Nick='$nick', Password='$password', URL='$url' where ID='$nr'") or exit("Błąd w zapytaniu: ");
+    if($nr==-1){
+
+        mysqli_query($serwer,"INSERT INTO `passtable`(`ID`, `Name`, `e-mail`, `Nick`, `Password`, `URL`)  VALUES (null,'$name','$email','$nick','$password','$url')" ) or exit("Błąd w zapytaniu");
+    }
+    else{
+        mysqli_query($serwer, "update passtable set Name='$name', `e-mail`='$email', Nick='$nick', Password='$password', URL='$url' where ID='$nr'") or exit("Błąd w zapytaniu: ");
+    }
     header("Location:panel.php");
 }
 if(isset($_POST['personaldataEdit'])) {
@@ -44,6 +50,12 @@ if(isset($_POST['personaldataEdit'])) {
     $Postcode = $_POST['Postcode'];
     $City = $_POST['City'];
     $Country = $_POST['Country'];
-    mysqli_query($serwer, "update personaldatatable set Name='$Name', SecondName='$SecondName', LastName='$LastName', PESEL='$PESEL', `e-mail`='$Email', PhoneNumber='$PhoneNumber', Adress='$Adress', Postcode='$Postcode', City='$City', Country='$Country' where ID='$nr'") or exit("Błąd w zapytaniu: ");
+    if($nr==-1){
+        mysqli_query($serwer,"INSERT INTO `personaldatatable`(ID`, `Name`, `SecondName`, `LastName`, `PESEL`, `e-mail`, `PhoneNumber`, `Adress`, `Postcode`, `City`, `Country`)  VALUES (null,'$name','$email','$nick','$password','$url') ) or exit("Błąd w zapytaniu");
+
+    }
+    else{
+        mysqli_query($serwer, "update personaldatatable set Name='$Name', SecondName='$SecondName', LastName='$LastName', PESEL='$PESEL', `e-mail`='$Email', PhoneNumber='$PhoneNumber', Adress='$Adress', Postcode='$Postcode', City='$City', Country='$Country' where ID='$nr'") or exit("Błąd w zapytaniu: ");
+    }
     header("Location:panel.php");
 }
